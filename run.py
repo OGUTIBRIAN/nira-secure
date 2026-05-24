f713a3834c6f48b53b2bee30ef12ff93c111ef2a
OGUTIBRIAN <oguti.brian@stud.umu.ac.ug>
2026-05-24 13:26:08 +0300

    Fix wallet route

--- run.py
+++ run.py
@@ -251,3 +251,9 @@ from flask import send_from_directory
 @app.route("/wallet")
 def wallet():
     return send_from_directory("static", "index.html")
+
+from flask import send_from_directory
+
+@app.route("/wallet")
+def wallet():
+    return send_from_directory("static", "index.html")
